e65ed5f93902641b49bf0ce0dfffa3ec700e77aa
runetaka <runetaka@gmail.com>
2022-09-10 15:07:40 +0900

    Initial Commit

---
+++ BoxProcessing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoxProcessing
+//
+//  Created by 池島孝浩 on 2022/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
